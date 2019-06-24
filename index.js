0261d2933d6ccdfac47629e9751c2e28c2bf78cf
Alexander Tran <alex.q.tran97@gmail.com>
2019-06-24 12:08:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var spy = "hello";
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+ console.log(spy.toUpperCase)
+}
